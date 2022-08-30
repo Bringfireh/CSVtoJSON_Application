df63b5dfb28dd9e9c80a30d4de9af2109059c00c
Bringfireh <ngutortsenongo@gmail.com>
2022-08-30 23:16:40 +0100

    modified the main method

--- CSVtoJSON_Application/Program.cs
+++ CSVtoJSON_Application/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using IronXL;
 
 namespace CSVtoJSON_Application
 {
@@ -10,6 +12,21 @@ namespace CSVtoJSON_Application
     {
         static void Main(string[] args)
         {
+            //var csvFilereader = new DataTable();
+            //csvFilereader = ReadExcel(csvFileName);
+        }
+        private DataTable ReadExcel(string fileName)
+        {
+            WorkBook workbook = WorkBook.Load(fileName);
+            
+            WorkSheet sheet = workbook.DefaultWorkSheet;
+            
+            return sheet.ToDataTable(true);
+        }
+        public void ReadCSVData(string csvFileName)
+        {
+            var csvFilereader = new DataTable();
+            csvFilereader = ReadExcel(csvFileName);
         }
     }
 }
