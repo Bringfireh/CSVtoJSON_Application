91bfd423153845cb10f750d81945dea33ac8c6e4
Bringfireh <ngutortsenongo@gmail.com>
2022-09-02 23:34:05 +0100

    modified the main method

--- CSVtoJSON_Application/Program.cs
+++ CSVtoJSON_Application/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,21 +13,33 @@ namespace CSVtoJSON_Application
     {
         static void Main(string[] args)
         {
-            //var csvFilereader = new DataTable();
-            //csvFilereader = ReadExcel(csvFileName);
-        }
-        private DataTable ReadExcel(string fileName)
-        {
-            WorkBook workbook = WorkBook.Load(fileName);
-            
+            string basedir = AppDomain.CurrentDomain.BaseDirectory;
+            string[] realpath = basedir.Split(new string[] { "bin", "Debug" }, StringSplitOptions.None);
+            string apppath = realpath[0];
+
+            //get the file name
+            string csvFileName = Path.Combine(apppath + "Data\\CSV", "input.csv");
+
+            //Read data into datatable;
+            DataTable csvFileReader = new DataTable();
+            WorkBook workbook = WorkBook.Load(csvFileName);
+
             WorkSheet sheet = workbook.DefaultWorkSheet;
+
+             csvFileReader = sheet.ToDataTable(true);
+        }
+        //public DataTable ReadExcel(string fileName)
+        //{
+        //    WorkBook workbook = WorkBook.Load(fileName);
             
-            return sheet.ToDataTable(true);
-        }
-        public void ReadCSVData(string csvFileName)
-        {
-            var csvFilereader = new DataTable();
-            csvFilereader = ReadExcel(csvFileName);
-        }
+        //    WorkSheet sheet = workbook.DefaultWorkSheet;
+            
+        //    return sheet.ToDataTable(true);
+        //}
+        //public void ReadCSVData(string csvFileName)
+        //{
+        //    var csvFilereader = new DataTable();
+        //    csvFilereader = ReadExcel(csvFileName);
+        //}
     }
 }
